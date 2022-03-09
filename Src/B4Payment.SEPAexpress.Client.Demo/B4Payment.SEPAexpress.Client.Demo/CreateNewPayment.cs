6c90abc03b60a612a4fe0e0a92eafaa201dcd901
Roman Motyka <roman.motyka@gmail.com>
2022-03-09 07:33:53 +0100

    feat: inital demo project

---
+++ Src/B4Payment.SEPAexpress.Client.Demo/B4Payment.SEPAexpress.Client.Demo/CreateNewPayment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B4Payment.SEPAexpress.Client.Demo
+{
+    internal class CreateNewPayment
+    {
+        public async Task ExecuteAsync()
+        {
+            var baseUrl = "https://sepaexpress-prod-fx.azurewebsites.net";
+            using var httpClient = new HttpClient();
+
+            var createPaymentRequest = new CreatePaymentHttpRequest
+            {
+                Amount = 10002,
+                MandateId = "3b8a541fffc0177e32a099d63227cb79",
+                CurrencyCode = "EUR",
+            };
+
+
+            var client = new Client(baseUrl, httpClient);
+
+            var createPaymentResponse = await client.PaymentsPOSTAsync(createPaymentRequest);
+
+
+        }
+    }
+}
---
+++ Src/B4Payment.SEPAexpress.Client.Demo/B4Payment.SEPAexpress.Client.Demo/Program.cs
@@ -0,0 +1,7 @@
+﻿using B4Payment.SEPAexpress.Client.Demo;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var createNewPayment = new CreateNewPayment();
+await createNewPayment.ExecuteAsync();
